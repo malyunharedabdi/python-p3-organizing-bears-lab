d0ca898a8e9bf3feda83b41ae457b83d5bc09426
malyunharedabdi <mankaharet48@gmail.com>
2026-05-21 05:42:33 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Mr. Chocolate", 10, "M", "Brown", "Calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Maria", 11, "F", "Red", "Calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Ahmed", 12, "M", "Black", "Calm", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Marwa", 15, "F", "Grey", "Calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Mr. Chocolate", 19, "M", "Purple", "Calm", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Abdi", 17, "M", "Brown", "Calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Qaalib", 7, "F", "Grey", "Sweet", 1);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES(NULL, 5, "F", "Black", "Quiet", 1);
